158b9a5bca297057d12c5ba41a442abe2407d779
codywood010 <codywood010@gmail.com>
2013-10-12 14:55:36 -0600

    Alphabetize data

--- platforms/ios/www/js/data.js
+++ platforms/ios/www/js/data.js
@@ -1,4 +1,80 @@
 var data = {
+
+"Apparel/Clothing Construction": {
+    "Polyester": 
+        ["Tailor Made, ProLock, Polyarn, OMNI", "http://www.superiorthreads.com/choose-the-right-thread-for-apparel/"],
+    "Silk":
+      ["Kimono Silk #100", "http://www.superiorthreads.com/product/brand/kimono-silk/"],
+    "Overlock/Serger Thread":
+      ["ProLock", "http://www.superiorthreads.com/product/Brand/prolock/"],  
+  },
+
+  "Appliqué": {
+    "Invisible Machine Appliqué":
+      ["#100 Kimono Silk, The Bottom Line, MonoPoly", "http://www.superiorthreads.com/shop/category/bottom-line/description/"],
+    "Decorative Machine Appliqué":
+      ["King Tut, Rainbows, New Brytes, Highlights, Art Studio Colors, #30 Silk, Nature Colors, Living Colors, LAVA, Superior Metallics, Glitter, So Fine #40, So Fine #30", "http://www.superiorthreads.com/embroidery-threads/"],
+    "Hand Appliqué":
+      ["The Bottom Line, #100 Kimono Silk, #50 Silk, MasterPiece, So Fine #50", "http://www.superiorthreads.com/shop/category/kimono-100-silk-thread/description/"],
+    "Fusible Appliqué":
+      ["Charlotteʼs Fusible Web", "http://www.superiorthreads.com/shop/category/charlottes-fusible-web/description/"]
+  },
+
+  "Bobbin Thread": {
+    "for Quilting": {
+      "Cotton":
+        ["Same as top thread or MasterPiece. MasterPiece is also available on prewound bobbins.", "http://www.superiorthreads.com/shop/category/masterpiece/description/"],
+      "Polyester":
+        ["Same as top thread, The Bottom Line, OMNI, or So Fine #50. (Also available as prewound bobbins)", "http://www.superiorthreads.com/shop/category/bottom-line/description/"],
+      "Invisible Monofilament":
+        ["MonoPoly polyester thread.", "http://www.superiorthreads.com/shop/category/monopoly/description/"]
+    },
+    "for Piecing": {
+      "Cotton":
+        ["MasterPiece. MasterPiece is also available on L-style prewound bobbins.", "http://www.superiorthreads.com/shop/category/masterpiece/description/"],
+      "Polyester":
+        ["We recommend cotton for piecing to accommodate high-temperature ironing.", "http://www.superiorthreads.com/shop/category/masterpiece/description/"]
+    },
+    "for Embroidery": {
+      "60 wt.":
+        ["The Bottom Line. The Bottom Line is also available on L-style prewound bobbins.", "http://www.superiorthreads.com/shop/category/bottom-line/description/"],
+      "50 wt.":
+        ["So Fine #50", "http://www.superiorthreads.com/shop/category/so-fine/description/"],
+      "40 wt.":
+        ["Highlights, Art Studio Colors, Nature Colors, Living Colors", "http://www.superiorthreads.com/embroidery-threads/"]
+    }
+  },
+  
+  
+  "Couching and Bobbin Work":
+    ["Halo, Razzle Dazzle, Silk Ribbon", "http://www.superiorthreads.com/shop/category/halo/description/"],
+
+  "Embroidery": {
+    "High Sheen": {
+      "Rayon":
+        ["Not recommended. Most rayon is weak and not colorfast.", "http://www.superiorthreads.com/embroidery-threads/"],
+      "Polyester":
+        ["Magnifco, Fantastico, Twist, Rainbows, Nature Color, Art Studio Colors, Living Colors, So Fine #40", "http://www.superiorthreads.com/embroidery-threads/"],
+      "Metallic":
+        ["Glitter, Superior Metallic", "http://www.superiorthreads.com/embroidery-threads/"]
+    },
+    "Matte Finish":
+      ["So Fine #40, So Fine #50, King Tut", "http://www.superiorthreads.com/embroidery-threads/"],
+    "Heavier/30 wt.":
+      ["New Brytes, So Fine #30, Lava", "http://www.superiorthreads.com/embroidery-threads/"],
+    "Glow-in-the-Dark":
+      ["NiteLite ExtraGlow", "http://www.superiorthreads.com/embroidery-threads/"],
+    "Metallic":
+      ["Superior Metallic or Glitter", "http://www.superiorthreads.com/embroidery-threads/"]
+  },
+
+  "Piecing": {
+    "Cotton":
+      ["MasterPiece", "http://www.superiorthreads.com/product/brand/masterpiece/"],
+    "Polyester":
+      ["We recommend cotton for piecing to acommodate high-temperature ironing. If you prefer polyester, use So Fine #50", "http://www.superiorthreads.com/product/brand/so-fine-50/"]
+  },
+
   "Quilting": {
     "Cotton": {
       "I want the thread to blend":
@@ -27,73 +103,7 @@ var data = {
     "Metallic":
       ["Superior Metallic, Glitter", "http://www.superiorthreads.com/shop/category/metallic-thread/description/"]
   },
-  "Bobbin Thread": {
-    "for Quilting": {
-      "Cotton":
-        ["Same as top thread or MasterPiece. MasterPiece is also available on prewound bobbins.", "http://www.superiorthreads.com/shop/category/masterpiece/description/"],
-      "Polyester":
-        ["Same as top thread, The Bottom Line, OMNI, or So Fine #50. (Also available as prewound bobbins)", "http://www.superiorthreads.com/shop/category/bottom-line/description/"],
-      "Invisible Monofilament":
-        ["MonoPoly polyester thread.", "http://www.superiorthreads.com/shop/category/monopoly/description/"]
-    },
-    "for Piecing": {
-      "Cotton":
-        ["MasterPiece. MasterPiece is also available on L-style prewound bobbins.", "http://www.superiorthreads.com/shop/category/masterpiece/description/"],
-      "Polyester":
-        ["We recommend cotton for piecing to accommodate high-temperature ironing.", "http://www.superiorthreads.com/shop/category/masterpiece/description/"]
-    },
-    "for Embroidery": {
-      "60 wt.":
-        ["The Bottom Line. The Bottom Line is also available on L-style prewound bobbins.", "http://www.superiorthreads.com/shop/category/bottom-line/description/"],
-      "50 wt.":
-        ["So Fine #50", "http://www.superiorthreads.com/shop/category/so-fine/description/"],
-      "40 wt.":
-        ["Highlights, Art Studio Colors, Nature Colors, Living Colors", "http://www.superiorthreads.com/embroidery-threads/"]
-    }
-  },
-  "Embroidery": {
-    "High Sheen": {
-      "Rayon":
-        ["Not recommended. Most rayon is weak and not colorfast.", "http://www.superiorthreads.com/embroidery-threads/"],
-      "Polyester":
-        ["Magnifco, Fantastico, Twist, Rainbows, Nature Color, Art Studio Colors, Living Colors, So Fine #40", "http://www.superiorthreads.com/embroidery-threads/"],
-      "Metallic":
-        ["Glitter, Superior Metallic", "http://www.superiorthreads.com/embroidery-threads/"]
-    },
-    "Matte Finish":
-      ["So Fine #40, So Fine #50, King Tut", "http://www.superiorthreads.com/embroidery-threads/"],
-    "Heavier/30 wt.":
-      ["New Brytes, So Fine #30, Lava", "http://www.superiorthreads.com/embroidery-threads/"],
-    "Glow-in-the-Dark":
-      ["NiteLite ExtraGlow", "http://www.superiorthreads.com/embroidery-threads/"],
-    "Metallic":
-      ["Superior Metallic or Glitter", "http://www.superiorthreads.com/embroidery-threads/"]
-  },
-  "Appliqué": {
-    "Invisible Machine Appliqué":
-      ["#100 Kimono Silk, The Bottom Line, MonoPoly", "http://www.superiorthreads.com/shop/category/bottom-line/description/"],
-    "Decorative Machine Appliqué":
-      ["King Tut, Rainbows, New Brytes, Highlights, Art Studio Colors, #30 Silk, Nature Colors, Living Colors, LAVA, Superior Metallics, Glitter, So Fine #40, So Fine #30", "http://www.superiorthreads.com/embroidery-threads/"],
-    "Hand Appliqué":
-      ["The Bottom Line, #100 Kimono Silk, #50 Silk, MasterPiece, So Fine #50", "http://www.superiorthreads.com/shop/category/kimono-100-silk-thread/description/"],
-    "Fusible Appliqué":
-      ["Charlotteʼs Fusible Web", "http://www.superiorthreads.com/shop/category/charlottes-fusible-web/description/"]
-  },
-  "Piecing": {
-    "Cotton":
-      ["MasterPiece", "http://www.superiorthreads.com/product/brand/masterpiece/"],
-    "Polyester":
-      ["We recommend cotton for piecing to acommodate high-temperature ironing. If you prefer polyester, use So Fine #50", "http://www.superiorthreads.com/product/brand/so-fine-50/"]
-  },
-
-"Apparel/Clothing Construction": {
-    "Polyester": 
-        ["Tailor Made, ProLock, Polyarn, OMNI", "http://www.superiorthreads.com/choose-the-right-thread-for-apparel/"],
-    "Silk":
-      ["Kimono Silk #100", "http://www.superiorthreads.com/product/brand/kimono-silk/"],
-    "Overlock/Serger Thread":
-      ["ProLock", "http://www.superiorthreads.com/product/Brand/prolock/"],  
-  },
+  
   "Serger": {
     "Decorative":
       ["King Tut, Rainbows, New Brytes, Highlights, Art Studio Colors, Nature Colors, Living Colors, LAVA, Superior Metallics, Glitter, Halo, Razzle Dazzle, So Fine #30, So Fine #40", "http://www.superiorthreads.com/serger-threads/"],
@@ -102,10 +112,8 @@ var data = {
     "For Fleece and Knits":
       ["ProLock, Polyarn ", "http://www.superiorthreads.com/product/Brand/prolock/"]
   },
-  "Couching and Bobbin Work":
-    ["Halo, Razzle Dazzle, Silk Ribbon", "http://www.superiorthreads.com/shop/category/halo/description/"],
-    
-      "Specialty Threads": {
+  
+  "Specialty Threads": {
     "Fusible Thread":
       ["Charlotte’s Fusible Web", "http://www.superiorthreads.com/product/Brand/charlottes-fusible-web/"],
     "Water Soluble Thread":
@@ -116,6 +124,7 @@ var data = {
       ["Kevlar", "http://www.superiorthreads.com/shop/category/?q=kevlar"]
   },
 
+
   "Upholstery Threads": {
     "Bonded Nylon":
       ["Superior Bonded Nylon", "http://www.superiorthreads.com/product/brand/bonded-nylon/"],
@@ -123,7 +132,6 @@ var data = {
       ["Solar Guard Bonded Polyester", "http://www.superiorthreads.com/product/brand/solar-guard/"]
   },
 
-    
     
 };
 
